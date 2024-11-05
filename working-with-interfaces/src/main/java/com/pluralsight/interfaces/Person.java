0c7443144ee329cb113b2a2106e081e02c573eaa
sirysiu <sirisew@gmail.com>
2024-11-05 10:39:39 -0500

    interface practice

---
+++ working-with-interfaces/src/main/java/com/pluralsight/interfaces/Person.java
@@ -0,0 +1,46 @@
+package com.pluralsight.interfaces;
+
+public class Person implements Comparable<Person> {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person { firstName='%s', lastName='%s', age=%d }\n", firstName, lastName, age);
+    }
+    @Override
+    public int compareTo(Person o) {
+        return Integer.compare(o.age, this.age);
+    }
+}
